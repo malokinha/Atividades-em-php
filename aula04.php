9b5f9eaa3450da843341a9c04ca98c89d0de7421
clarinha <169086059+malokinha@users.noreply.github.com>
2024-07-30 14:32:11 -0300

    Tabela de fotos com PHP

--- aula04.php
+++ aula04.php
@@ -11,20 +11,38 @@
         td{
             border: 1px solid black;
         }
+        img{
+            width: 250px;
+        }
+        body{
+            background-color: #cc0099;
+        }
     </style>
 </head>
 <body>
     <?php 
     echo "<h1>galeria de fotos</h1>";
     echo"<table><tr>";
+    for($x=1; $x<=5; $x++){
+    echo"<td><img src='img/divertidamente$x.jpeg'></td>";
+    }
+    echo "</tr>";
+    echo "<tr>";
+    for($x=6; $x<=10; $x++){
+        echo"<td><img src='img/divertidamente$x.jpeg'></td>";
+        }
+    echo "</tr></table>";
+
+
+    echo "<h1>Tabela de 1 a10 em PHP</h1>";
+    echo"<table><tr>";
     for($x=1; $x<=10; $x++){
         echo"<td>$x</td>";
     }
     echo "</tr></table>";
-    ?>
-    
-    <img src="" alt="">
 
+
+    ?>
 
 </body>
 </html>
