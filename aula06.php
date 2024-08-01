779ce3e9a118c03471994d3be939baee2b6d441b
clarinha <169086059+malokinha@users.noreply.github.com>
2024-08-01 14:50:13 -0300

    Add files via upload

---
+++ aula06.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<, initial-scale=1.0">
+    <title>Aula 06 PHP</title> 
+
+</head>  
+<body>
+    <?php
+    echo"<h1>Arrays - Vetores</h1>";
+    $carros= array('Fiesta','Palio','Coesa','siena');
+    echo $carros[1], "<br>";
+    echo $carros[3], "<br>";
+
+    ?>
+</body>
+</html>
