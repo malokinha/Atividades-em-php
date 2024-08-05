9c77f770e51745963e57d9b43e0f4867f6f91d67
clarinha <169086059+malokinha@users.noreply.github.com>
2024-08-05 18:12:28 -0300

    Add files via upload

--- aula06.php
+++ aula06.php
@@ -2,16 +2,41 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=<, initial-scale=1.0">
-    <title>Aula 06 PHP</title> 
-
-</head>  
+    <meta name="viewport" content="width=], initial-scale=1.0">
+    <title>Aula 06 php</title>
+</head>
 <body>
-    <?php
-    echo"<h1>Arrays - Vetores</h1>";
-    $carros= array('Fiesta','Palio','Coesa','siena');
+    <?php 
+    echo "<h1>Arrays - Vetores</h1>";
+    $carros = array('Fiesta','palio','corsa','siena');
     echo $carros[1], "<br>";
     echo $carros[3], "<br>";
+    //Adicionando novos elementos 
+   $carros[4] = "Clío";
+   $carros[11] = "Versa";
+   $carros[ ] = "Sandero";
+   echo $carros[4] , "<br>"; //Resultado Clío
+   echo $carros[5] , "<br>"; //Sem Resultado 
+   echo $carros[11] , "<br>"; //Resultado varsa 
+   echo $carros[12] , "<br>"; //Resultados Sandero 
+
+   $carros["s1"] = "Siena"; //Usando string como indice 
+   echo $carros["s1"] , "<br>"; //Reesutado Siena 
+   echo $carros[13], "<br>";
+   $carros[13] = "Ferrari";
+   echo $carros[13], "<br>";
+   $carros [5] = "Fusca";
+
+   echo"<h1>Impressão completa da Array</h1>";
+
+   echo "</pre>"; 
+   print_r($carros);
+   echo"<pre>";
+
+   echo "<pre>";
+   var_dump($carros);
+   echo "</pres>";
+   
 
     ?>
 </body>
---
+++ aula07.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width="initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a hret="recebe-aula07.php?">Enviar o nome Clara</a>
+
+    <form action= "recebe-aula07.php">
+         <label for="nome">Nome: </label><br>
+         <input type="text" name="nome"><br>
+         <label for="sobrenome">sobrenome: </label><br>
+         <input type="text" name="sobrenome"><br>
+         <input type="submit" value="enviar">
+   </form>
+</body>
+</html>
